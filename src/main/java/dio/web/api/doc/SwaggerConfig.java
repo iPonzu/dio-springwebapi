0137aee7129d1a203bb441856feb024265a3a8d1
iPonzu <joao_ignaciocastro1603@hotmail.com>
2025-05-23 21:29:42 -0300

    swagger-ui responding

--- src/main/java/dio/web/api/doc/SwaggerConfig.java
+++ src/main/java/dio/web/api/doc/SwaggerConfig.java
@@ -1,51 +1,54 @@
 package dio.web.api.doc;
 
-import java.util.HashSet;
 import java.util.Arrays;
+import java.util.HashSet;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-    private Contact contato(){
+public class SwaggerConfig {    
+    private Contact contato() {
         return new Contact(
-            "CharmKMagazine",
-            "https://charmkmagazine.com.br",
-            "myemail@email.com"
-        );
+                "Seu nome",
+                "http://www.seusite.com.br",
+                "voce@seusite.com.br");
     }
-    private ApiInfoBuilder informacoesApi(){
+    private ApiInfoBuilder informacoesApi() {
+
         ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-        apiInfoBuilder.title("API REST");
-        apiInfoBuilder.description("API REST de uso de Springboot REST API");
+
+        apiInfoBuilder.title("Title - Rest API");
+        apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
         apiInfoBuilder.version("1.0");
-        apiInfoBuilder.termsOfServiceUrl("Termos de uso: Termos de uso");
-        apiInfoBuilder.license("Licença - CharmKMagazine");
-        apiInfoBuilder.licenseUrl("https://charmkmagazine.com.br");
+        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
+        apiInfoBuilder.license("Licença - Sua Empresa");
+        apiInfoBuilder.licenseUrl("http://www.seusite.com.br");
         apiInfoBuilder.contact(this.contato());
+
         return apiInfoBuilder;
+
     }
     @Bean
-    public Docket detalheApi(){
+    public Docket detalheApi() {
         Docket docket = new Docket(DocumentationType.SWAGGER_2);
-        
+
         docket
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("pacote.comseus.controllers"))
-            .paths(PathSelectors.any())
-            .build()
-            .apiInfo(this.informacoesApi().build())
-            .consumes(new HashSet<String>(Arrays.asList("application/json")))
-            .produces(new HashSet<String>(Arrays.asList("application/json")));
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("dio.web.api.controller")) //package dio.web.api.controller
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.informacoesApi().build())
+                .consumes(new HashSet<String>(Arrays.asList("application/json")))
+                .produces(new HashSet<String>(Arrays.asList("application/json")));
 
         return docket;
     }
